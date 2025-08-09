c6854f84b46385d8c7621126b0dc11edd66c1d44
Abdul Hadi <khabdulhadis@gmail.com>
2025-08-09 17:28:03 +0500

    added CORSE middleware In production, specifiy allowed origins

--- app/main.py
+++ app/main.py
@@ -23,3 +23,11 @@ app = FastAPI(
 limiter = Limiter(key_func=get_remote_address)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
